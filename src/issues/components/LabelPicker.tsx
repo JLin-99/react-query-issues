13e95649020c1053f742f0d8a0cc0d79037264c6
José Lin (Light-it) <jose.lin@lightit.io>
2023-05-18 05:44:58 -0300

    feature: add React Query with custom hook

---
+++ src/issues/components/LabelPicker.tsx
@@ -0,0 +1,39 @@
+import { FC } from "react";
+import { LoadingIcon } from "../../shared/components/LoadingIcon";
+import { useLabels } from "../hooks/useLabels";
+
+interface LabelPickerProps {
+  selectedLabels: string[];
+  onChange: (labelName: string) => void;
+}
+
+export const LabelPicker: FC<LabelPickerProps> = ({
+  selectedLabels,
+  onChange,
+}) => {
+  const labelsQuery = useLabels();
+
+  if (labelsQuery.isLoading) {
+    return <LoadingIcon />;
+  }
+
+  return (
+    <>
+      {labelsQuery.data?.map((label) => (
+        <span
+          key={label.id}
+          className={`badge rounded-pill m-1 label-picker ${
+            selectedLabels.includes(label.name) ? "label-active" : ""
+          }`}
+          style={{
+            border: `1px solid #${label.color}`,
+            color: `#${label.color}`,
+          }}
+          onClick={() => onChange(label.name)}
+        >
+          {label.name}
+        </span>
+      ))}
+    </>
+  );
+};
